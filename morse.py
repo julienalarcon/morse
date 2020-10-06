1a9e3df0dbf18103cfa2afcc2a786a83d527ef29
Julien ALARCON <alarcon.julien@gmail.com>
2020-10-06 15:16:48 +0200

    First mplementation

--- morse.py
+++ morse.py
@@ -1,4 +1,33 @@
 # pylint: disable=missing-docstring
 
+ALPHABET = {
+    '.-':   'A',
+    '-...': 'B',
+    '-.-.': 'C',
+    '-..':  'D',
+    '.':    'E',
+    '..-.': 'F',
+    '--.':  'G',
+    '....': 'H',
+    '..':   'I',
+    '.---': 'J',
+    '-.-':  'K',
+    '.-..': 'L',
+    '--':   'M',
+    '-.':   'N',
+    '---':  'O',
+    '.--.': 'P',
+    '--.-': 'Q',
+    '.-.':  'R',
+    '...':  'S',
+    '-':    'T',
+    '..-':  'U',
+    '...-': 'V',
+    '.--':  'W',
+    '-..-': 'X',
+    '-.--': 'Y',
+    '--..': 'Z'
+}
+
 def decode(message):
-    pass # TODO: implement the behavior!
+    return ''.join([ALPHABET[s] for s in message.split(" ") if s in ALPHABET])
